9d0fe09286cd58ae3c3346ca53c9606fca4179d9
dleottau <ing.leottau@gmail.com>
2024-06-27 00:27:39 -0400

    Update SD.ino

--- seguidorLinea_IR/SD.ino
+++ seguidorLinea_IR/SD.ino
@@ -16,8 +16,13 @@ void setup_SD() {
   if (!SD.exists("dataKKs.csv")) {
     myFile = SD.open("dataKKs.csv", FILE_WRITE);
     if (myFile) {
+<<<<<<< Updated upstream
       //myFile.println("Fecha,Hora,sDiametro,Diametro,Vueltas,Consumo,DmSeg,sLinea(cms)");
       myFile.println("Fecha,Hora,%Rollo,Radio,Vueltas,Uso(cms)");
+=======
+       myFile.println("Fecha,Hora,sDiametro,Diametro,Vueltas,Consumo,dTmS,sLinea");
+      //myFile.println("Fecha,Hora,%Rollo,Radio,Vueltas,Uso(cms)");
+>>>>>>> Stashed changes
       Serial.println("archivo main creado");
     } else {
       Serial.println("Fallo de apertura main");
