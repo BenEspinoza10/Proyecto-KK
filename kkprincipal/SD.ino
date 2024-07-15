4b591cf8d13b2dd4a0a5993c496ba999aba407c5
BenEspinoza10 <50683392+BenEspinoza10@users.noreply.github.com>
2024-07-15 18:03:01 -0400

    Renombramiento de archivos
    Ahora al momento de crear un id nuevo en un dispositivo, guarda cualquier archivo antiguo que haya con un nombre diferente y crea uno nueva

--- kkprincipal/SD.ino
+++ kkprincipal/SD.ino
@@ -3,7 +3,7 @@
 void setup_SD() {
   if (!SD.begin(chipSelect)) {
     Serial.println("initialization of SD failed!");
-    while (true){
+    while (true) {
       blink_led_red(3, 200);
       blink_led_blue(1, 200);
       delay(10000);
@@ -11,18 +11,18 @@ void setup_SD() {
     return;
   }
 
-//escritura inicial de cabecera del archivo csv
+  //escritura inicial de cabecera del archivo csv
   if (!SD.exists("dataKKs.csv")) {
     File myFile = SD.open("dataKKs.csv", FILE_WRITE);
-    if (myFile) {      
+    if (myFile) {
       myFile.print("id dispositivo: ");
-      myFile.println(id);      
+      myFile.println(id);
       myFile.println("Fecha,Hora,sDist,sDist2,sDiam,Giros,DtSeg");
       myFile.close();
       Serial.println("archivo main creado");
     } else {
       Serial.println("Fallo de apertura main");
-      while (true){
+      while (true) {
         blink_led_red(2, 200);
         blink_led_blue(2, 200);
         delay(10000);
@@ -30,13 +30,52 @@ void setup_SD() {
       return;
     }
   } else {
+    //se detecta que ya existe un archivo
     Serial.println("Archivo main ya existe");
+    int i = 1;
+    String newFileName;
+    //se renombra con un numero unico, en caso de haber varios archivos antiguos, ni uno se pierda
+    do {
+      newFileName = "dataKKsold" + String(i) + ".csv";
+      i++;
+    } while (SD.exists(newFileName));
+
+    //se hace el renombramiento
+    if (SD.rename("dataKKs.csv", newFileName.c_str())) {
+      Serial.println("Archivo renombrado a " + newFileName);
+    } else {
+      Serial.println("Fallo al renombrar el archivo");
+      while (true) {
+        blink_led_red(2, 200);
+        blink_led_blue(2, 200);
+        delay(10000);
+      }
+      return;
+    }
+
+    //se crea un nuevo archivo con el id del dispositivo
+    File myFile = SD.open("dataKKs.csv", FILE_WRITE);
+    if (myFile) {
+      myFile.print("id dispositivo: ");
+      myFile.println(id);
+      myFile.println("Fecha,Hora,sDist,sDist2,sDiam,Giros,DtSeg");
+      myFile.close();
+      Serial.println("archivo main creado");
+    } else {
+      Serial.println("Fallo de apertura main");
+      while (true) {
+        blink_led_red(2, 200);
+        blink_led_blue(2, 200);
+        delay(10000);
+      }
+      return;
+    }
   }
 }
 
 void escritura_SD() {
   File myFile = SD.open("dataKKs.csv", FILE_WRITE);
-  if (myFile) {  
+  if (myFile) {
     myFile.println("");
     myFile.print(now.year(), DEC);
     myFile.print('/');
@@ -49,20 +88,20 @@ void escritura_SD() {
     myFile.print(now.minute(), DEC);
     myFile.print(':');
     myFile.print(now.second(), DEC);
-    myFile.print(",");    
-    myFile.print(sensorDiametro,1);
-    myFile.print(",");    
-    myFile.print(sensorDiametro2,1);
     myFile.print(",");
-    myFile.print(diametro,1);
-    myFile.print(",");    
-    myFile.print(vueltas_temp,3);
+    myFile.print(sensorDiametro, 1);
+    myFile.print(",");
+    myFile.print(sensorDiametro2, 1);
+    myFile.print(",");
+    myFile.print(diametro, 1);
+    myFile.print(",");
+    myFile.print(vueltas_temp, 3);
     //myFile.print(",");
     //myFile.print(gasto_temp,4);
     myFile.close();
   } else {
     Serial.println("Error abriendo archivo en la SD main");
-    while (true){
+    while (true) {
       blink_led_red(3, 200);
       blink_led_blue(2, 200);
       delay(10000);
@@ -73,21 +112,20 @@ void escritura_SD() {
 
 void escritura_SD_temp() {
   File myFile = SD.open("dataKKs.csv", FILE_WRITE);
-  //DateTime nowtemp = rtc.now(); 
-  if (myFile) {    
-    myFile.print(","); 
-    myFile.print(double(millis()-cronometro)/1000.0,3);       
-    //myFile.print(",");   
+  //DateTime nowtemp = rtc.now();
+  if (myFile) {
+    myFile.print(",");
+    myFile.print(double(millis() - cronometro) / 1000.0, 3);
+    //myFile.print(",");
     //myFile.print(sensorLinea,DEC);
     myFile.close();
   } else {
     Serial.println("Error abriendo archivo en la SD temp");
-    while (true){
+    while (true) {
       blink_led_red(4, 200);
       blink_led_blue(1, 200);
       delay(10000);
     }
     return;
   }
-
 }
