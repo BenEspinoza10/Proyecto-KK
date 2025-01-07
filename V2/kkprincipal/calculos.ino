6626509e676cf7e1eb013e4e28aef922ac30febd
BenEspinoza10 <50683392+BenEspinoza10@users.noreply.github.com>
2025-01-07 15:48:15 -0300

    Avances

---
+++ V2/kkprincipal/calculos.ino
@@ -0,0 +1,67 @@
+void calculo() {
+
+  unsigned long timeout_dTmax=T_AVG_HALADA; //almacena el tiempo más grande de sensado en cada línea
+  double dt_avg=0; //almacena el promedio de tiempo de sensado en cada línea
+  bool vibra=0; //flag q indica si el rollo está vibrando por los giros o no
+  cronometro = millis();
+  cronometro_v=millis();
+
+  
+  while (flag_rolling == 1) {
+
+    // Leer sensor de vibración, 1 si vibra, 0 si no está vibrando
+    vibra=MeasureDigitalN(SAMPLES, IN_INTERRUPT);
+    if (vibra==1) {
+      cronometro_v = millis(); // si aún está vibrando reiniciar medición de tiempo de vibración
+    }
+    //Leer y filtrar puerto del IR, si es ruido salir    
+    huella = MeasureTurnCount(T_FILTER, IN_LINE);  //Sensa durante 1.0 mS para filtrar ruido
+    if(huella==-1){
+      //aqui hay que hacer otra cosa en vez de retornar
+      continue;
+    }
+    huellatemp = huella;
+    bool fin_halada=0;
+    if (huella1temp != huellatemp and (huellatemp!=-1 or huella1temp!=-1)) {
+      vueltas_temp++;
+
+      //calcula dT de la línea anterior
+      dT_line=millis()-cronometro;
+
+      fin_halada = ( dT_line>timeout_dTmax*1.5 || dt_avg>T_AVG_HALADA*3 || dT_line>T_MAX_HALADA || millis()-cronometro_v > T_MAX_HALADA);
+      
+      if(!fin_halada){ // si gira, vibra y el Dt de giro fue corto
+        if(dT_line>timeout_dTmax) timeout_dTmax = dT_line; // tiempo más alto en que se detecta una transición de linea
+        dt_avg = dt_avg*(vueltas_temp-1.0)/vueltas_temp + double(dT_line)/vueltas_temp;  //promedia y acumula cada muestra
+        if(ENABLE_SD) escritura_SD_temp();  //solo escribe acá si hace parte de la misma halada
+      }
+      cronometro = millis();
+      huella1temp = huellatemp;
+      vibra=1;
+      //led_green_set(huella); 
+    }
+    if (fin_halada) {
+      escribe_fin_halada();
+    }else if(millis()-cronometro > T_SLEEP*1000 /*&& millis()-cronometro_v > T_SLEEP*1000*/){
+      // Tras T_SLEEP segundos de inactividad lee sensor de distancia, escribe en SD y se va a mimir
+      sensorDiametro2 = MeasureAnalogN(SAMPLES,IN_DIAMETER);
+      escribe_fin_halada();
+    }
+  } 
+}
+
+void escribe_fin_halada(){
+  //fin evento halada de papel, se hacen los cálculos respectivos, actualmente está definido con 2 segundos      
+  vueltas_temp = vueltas_temp / n_octocoplador * 0.5;
+  
+  sensorDiametro = MeasureAnalogN(SAMPLES,IN_DIAMETER);  
+
+  //escritura de datos en la tarjeta SD
+  if(ENABLE_SD) escritura_SD();
+  else print_temporal_tirada();
+
+  vueltas_temp = 0;  
+  flag_rolling = 0;
+  sensorDiametro2=0;
+  blink_led_green(2,100); 
+}
---
+++ V2/kkprincipal/config.ino
@@ -0,0 +1,132 @@
+#include "headers.h"
+
+//codigo que corre al inicio del codigo, si se aprieta el boton dentro de los primeros 10 segundos mientras la led está amarilla, se entra en modo condiguracion, en caso contrario, se cargan los datos desde la memoria.
+int espera_configuracion() {
+  Serial.println("Esperando configuración");
+  //led_yellow_on();
+  unsigned long tiempoConfig = millis();
+  while (millis() - tiempoConfig < T_CONFIG) {  //se espera el input del boton por 10 seg
+    led_yellow_on_t(10);
+    if (digitalRead(PUSH_BUTTON) == 0) {
+      while (digitalRead(PUSH_BUTTON) == 0)
+        ;
+      configuracion();
+      return 1;
+    }
+  }
+  led_off();
+  return 0;
+}
+
+void configuracion() {
+  led_off();
+  
+  pinMode(OUT_DISABLE_POWER, OUTPUT);  //Se pone el pin en escritura
+  digitalWrite(OUT_DISABLE_POWER, LOW);
+
+  //primero poner el rollo en la línea negra
+  led_blue_on();
+  while (digitalRead(PUSH_BUTTON) == 1){
+    Serial.print("Ingrese negro: ");
+    Serial.println(analogRead(IN_LINE));
+    }
+  led_off();
+  int negro = calcular_promedio(IN_LINE);
+  led_off();
+  led_blue_on();
+  while (digitalRead(PUSH_BUTTON) == 0)
+    Serial.println(analogRead(IN_LINE));
+
+  //luego hay que poner el valor del blanco
+  while (digitalRead(PUSH_BUTTON) == 1){
+    Serial.print("Ingrese blanco: ");
+    Serial.println(analogRead(IN_LINE));
+  }
+  led_off();
+  int blanco = calcular_promedio(IN_LINE);
+
+  //se calcula el umbral
+  umbral = (blanco + negro) / 2;
+  while (digitalRead(PUSH_BUTTON) == 0)
+    ;
+  led_off();
+  led_blue_on();
+
+  //luego hay que poner el rollo más grande posible
+  while (digitalRead(PUSH_BUTTON) == 1){
+    Serial.print("Ingrese rollo nuevo: ");
+    Serial.println(analogRead(IN_DIAMETER));
+  }
+  led_off();
+
+
+  radio_max_analog = calcular_promedio(IN_DIAMETER);
+  while (digitalRead(PUSH_BUTTON) == 0)
+    ;
+  led_off();
+  led_blue_on();
+
+  //por ultimo hay que poner un rollo vacío
+  while (digitalRead(PUSH_BUTTON) == 1){
+    Serial.print("Ingrese rollo vacío: ");
+    Serial.println(analogRead(IN_DIAMETER));
+  }
+  led_off(); 
+  radio_min_analog = calcular_promedio(IN_DIAMETER);
+  led_off();
+  led_blue_on();
+  while (digitalRead(PUSH_BUTTON) == 0)
+    ;
+
+  Serial.println("negro,blanco,umbral; radio min; radiomax ");
+  Serial.print(negro);
+  Serial.print(" ; ");
+  Serial.print(blanco);
+  Serial.print(" ; ");
+  Serial.print(umbral);
+  Serial.print(" ; ");
+  Serial.print(radio_max_analog);
+  Serial.print(" ; ");
+  Serial.println(radio_min_analog);
+
+  escribir_memoria(umbral, radio_max_analog, radio_min_analog);  //se guardan los valores en memoria eeprom
+}
+
+int calcular_promedio(int pin) {
+  long suma = 0;
+  for (int i = 0; i < 100; i++) {
+    //led_yellow_on();
+    suma += analogRead(pin);
+    led_yellow_on_t(10);
+    led_off();
+    delay(10);
+  }
+  return suma / 100;
+}
+
+void escribir_memoria(int umbral, int radio_max, int radio_min) {
+  escribirEEPROM(0, umbral);
+  escribirEEPROM(2, radio_max);
+  escribirEEPROM(4, radio_min);
+}
+
+
+void escribirEEPROM(uint8_t address, int value) {
+  // Descomponer el int en bytes individuales
+  uint8_t bytes[2];
+  bytes[0] = (value >> 8) & 0xFF;
+  bytes[1] = value & 0xFF;
+
+  // Escribir los bytes en la NVRAM
+  rtc.writenvram(address, bytes, 2);
+}
+
+int leerEEPROM(uint8_t address) {
+  // Leer los 2 bytes de la NVRAM
+  uint8_t bytes[2];
+  rtc.readnvram(bytes, 2, address);
+
+  // Recomponer los bytes en un int
+  int value = ((int)bytes[0] << 8) | bytes[1];
+  return value;
+}
